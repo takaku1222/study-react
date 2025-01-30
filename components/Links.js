c910e359645a28ac778cf81e74b64a1e39ac362e
takaku1222 <tomo15161222@icloud.com>
2025-01-30 15:46:45 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,32 +1,32 @@
-import Image from "next/image";
 import styles from "./Links.module.css";
+
+const ITEMS = [
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy now",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Read our docs",
+  },
+];
 
 export function Links() {
   return (
     <div className={styles.ctas}>
-      <a
-        className={styles.primary}
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          className={styles.logo}
-          src="/vercel.svg"
-          alt="Vercel logomark"
-          width={20}
-          height={20}
-        />
-        Deploy now
-      </a>
-      <a
-        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={styles.secondary}
-      >
-        Read our docs
-      </a>
+      {ITEMS.map((item) => {
+        return (
+          <a
+            key={item.href}
+            href={item.href}
+            className={styles.primary}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>{item.title}</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
