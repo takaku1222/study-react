907c31d0921c2cf786177607f06768a516bafb25
takaku1222 <tomo15161222@icloud.com>
2025-02-01 14:53:44 +0900

    ✨ 文字列と真偽値のuseState導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,12 +18,26 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "rebeccapurple";
@@ -44,8 +58,10 @@ export default function Home() {
         <Header />
         <Main page="index" />
         <Footer />
-        <h1>{count}</h1>
+        {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+        <input type="text" value={text} onChange={handleChange} />
       </div>
     </>
   );
