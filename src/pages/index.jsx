414bd8e396752c6fe2d35f2bf0db058043d3d06c
takaku1222 <tomo15161222@icloud.com>
2025-02-01 14:11:27 +0900

    ⚡️ useCallback,useEffectの第2引数追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,14 +19,14 @@ const geistMono = Geist_Mono({
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "rebeccapurple";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
