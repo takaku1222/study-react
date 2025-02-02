daa89e4317d3e2106beb71d39f63a4f5cdd2b0ad
takaku1222 <tomo15161222@icloud.com>
2025-02-02 14:19:21 +0900

    ✨ useRouter導入

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+
+export const useBgColor = () => {
+  const router = useRouter();
+  console.log(router);
+
+  const bgColor = useMemo(() => {
+    switch (router.pathname) {
+      case "/": {
+        return "rebeccapurple";
+      }
+      case "/about": {
+        return "black";
+      }
+      default: {
+        return "";
+      }
+    }
+  }, [router.pathname]);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = bgColor;
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, [bgColor]);
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,12 @@ import "src/styles/globals.css";
 import Head from "next/head";
 import { useCounter } from "src/hooks/useCounter";
 import { useInputArray } from "src/hooks/useInputArray";
-import { useBgRebeccapurple } from "src/hooks/useBgRebeccapurple";
+import { useBgColor } from "src/hooks/useBgColor";
 
 function MyApp({ Component, pageProps }) {
   const counter = useCounter();
   const inputArray = useInputArray();
-  useBgRebeccapurple();
+  useBgColor();
 
   return (
     <>
