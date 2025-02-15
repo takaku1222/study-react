8506cf83a31890ffc501e169acc9d330ad7e5ed1
takaku1222 <tomo15161222@icloud.com>
2025-02-15 11:08:05 +0900

    ♻️ 配列のデータのAPIを叩くときの処理を共通化

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,5 +1,5 @@
-import { useComments } from "src/hooks/useComments";
 import Link from "next/link";
+import { useComments } from "src/hooks/useFetchArray";
 
 export const CommentsComponent = () => {
   const { data, error, isLoading, isEmpty } = useComments();
