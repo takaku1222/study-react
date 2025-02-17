c45ac076eb11bd4f63915bb999b5a6b9c4085a8d
takaku1222 <tomo15161222@icloud.com>
2025-02-17 11:11:23 +0900

    ✨ コメントページで元の投稿を表示

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,3 +1,4 @@
+import { PostByCommentId } from "src/components/Post/PostByCommentId";
 import { useComment } from "src/hooks/useComment";
 
 export const CommentComponent = () => {
@@ -18,6 +19,8 @@ export const CommentComponent = () => {
         <li>{data.name}</li>
         <li>{data.email}</li>
       </ul>
+      <h2>元の記事</h2>
+      <PostByCommentId id={data.postId} />
     </div>
   );
 };
