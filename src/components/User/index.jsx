f255720bf9516e339306afb79a9877761ba4f788
takaku1222 <tomo15161222@icloud.com>
2025-02-17 10:47:11 +0900

    ✨ ユーザーページに投稿一覧を追加

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -1,3 +1,4 @@
+import { PostsByUserId } from "src/components/Posts/PostsByUserId";
 import { useUser } from "src/hooks/useUser";
 
 export const UserComponent = () => {
@@ -14,6 +15,7 @@ export const UserComponent = () => {
   return (
     <div>
       <h1>{data.name}</h1>
+      <h2>詳細</h2>
       <ul>
         <li>{data.email}</li>
         <li>{data.username}</li>
@@ -22,6 +24,9 @@ export const UserComponent = () => {
         <li>{data.website}</li>
         <li>{data.company.name}</li>
       </ul>
+      <h2>投稿</h2>
+      <PostsByUserId id={data.id} />
+      <h2>コメント</h2>
     </div>
   );
 };
