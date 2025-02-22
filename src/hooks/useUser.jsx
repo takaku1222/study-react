e068bfa07234e6653dd64555c31536ef0954fcbf
takaku1222 <tomo15161222@icloud.com>
2025-02-23 07:24:00 +0900

    ユーザー一覧と投稿でSSR実装

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,10 +1,10 @@
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 import { useRouter } from "next/router";
 import { fetcher } from "src/utils/fetcher";
 
 export const useUser = () => {
   const router = useRouter();
-  const { data, error } = useSWR(
+  const { data, error } = useSWRImmutable(
     router.query.id
       ? `https://jsonplaceholder.typicode.com/users/${router.query.id}`
       : null,
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,15 +1,31 @@
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { UsersComponent } from "src/components/Users";
+import { SWRConfig } from "swr";
 
-const Users = () => {
+export const getServerSideProps = async () => {
+  const USERS_API_URL = `https://jsonplaceholder.typicode.com/users`;
+  const users = await fetch(USERS_API_URL);
+  const usersData = await users.json();
+
+  return {
+    props: {
+      [USERS_API_URL]: usersData,
+    },
+  };
+};
+
+const Users = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Posts Page</title>
       </Head>
-      <Header />
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </div>
   );
 };
